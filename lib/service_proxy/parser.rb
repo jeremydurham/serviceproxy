e5fccaa68fcfe21a10eddcd0dacf416f6cf87167
Jeremy Durham <jeremydurham@gmail.com>
2009-12-03 16:38:16 -0500

    Bumped version to 0.2.1 and added basic support for bindings; removed Hpricot dependency

--- lib/service_proxy/parser.rb
+++ lib/service_proxy/parser.rb
@@ -1,26 +1,35 @@
 module ServiceProxy
   class Parser < Nokogiri::XML::SAX::Document
-    attr_accessor :wsdl_namespace, :soap_namespace, :target_namespace, :service_methods, :soap_actions
+    attr_accessor :wsdl_namespace, :soap_namespace, :target_namespace, :service_methods, :soap_actions, :binding
     
     def initialize(*args)
       self.service_methods = []
       self.soap_actions = Hash.new('')
+      self.binding = false
       super
     end
     
-    def start_element_namespace(name, attributes, prefix, uri, namespace)      
+    def start_element_namespace(name, attributes, prefix, uri, namespace)
       case name
+        when 'binding'
+          self.binding = true
         when 'definitions'
           self.wsdl_namespace = prefix if uri == 'http://schemas.xmlsoap.org/wsdl/'
           self.soap_namespace = prefix if uri == 'http://schemas.xmlsoap.org/wsdl/soap/'
           attribute = attributes.find { |attribute| attribute.localname == 'targetNamespace' }
           self.target_namespace = attribute.value if attribute
         when "operation"
-          service_method = attributes.first.value if prefix == self.wsdl_namespace
-          soap_action = attributes.find { |attribute| attribute.localname == 'soapAction' }
-          self.soap_actions[self.service_methods.last] = soap_action.value if soap_action          
-          (self.service_methods << service_method unless self.service_methods.include?(service_method)) if service_method
+          if self.binding
+            service_method = attributes.first.value if prefix == self.wsdl_namespace
+            soap_action = attributes.find { |attribute| attribute.localname == 'soapAction' }
+            self.soap_actions[self.service_methods.last] = soap_action.value if soap_action
+            (self.service_methods << service_method unless self.service_methods.include?(service_method)) if service_method
+          end
       end
-    end    
+    end
+    
+    def end_element_namespace(name, prefix, uri)
+      self.binding = false if name == 'binding' && prefix == self.wsdl_namespace
+    end
   end
 end
--- serviceproxy.gemspec
+++ serviceproxy.gemspec
@@ -2,11 +2,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{serviceproxy}
-  s.version = '0.2.0'
+  s.version = '0.2.2'
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Jeremy Durham"]
-  s.date = %q{2009-10-25}
+  s.date = %q{2009-12-03}
   s.description = %q{Lightweight SOAP library for Ruby}
   s.email = %q{jeremydurham@gmail.com}
   s.extra_rdoc_files = ["README", "LICENSE"]
@@ -25,16 +25,13 @@ Gem::Specification.new do |s|
 
     if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
       s.add_runtime_dependency(%q<nokogiri>, [">= 0"])
-      s.add_runtime_dependency(%q<hpricot>, [">= 0"])
       s.add_runtime_dependency(%q<builder>, [">= 0"])
     else
       s.add_dependency(%q<nokogiri>, [">= 0"])
-      s.add_dependency(%q<hpricot>, [">= 0"])
       s.add_dependency(%q<builder>, [">= 0"])
     end
   else
     s.add_dependency(%q<nokogiri>, [">= 0"])
-    s.add_dependency(%q<hpricot>, [">= 0"])
     s.add_dependency(%q<builder>, [">= 0"])
   end
 end
