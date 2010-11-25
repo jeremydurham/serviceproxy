8279479433f3c01e556120a110952e3d9d8e2105
Jeremy Durham <jeremydurham@gmail.com>
2010-11-25 15:00:21 -0500

    Added ServiceProxy::VERSION and service_proxy.rb

---
+++ lib/service_proxy.rb
@@ -0,0 +1,3 @@
+require File.join(File.dirname(__FILE__), 'service_proxy', 'base')
+require File.join(File.dirname(__FILE__), 'service_proxy', 'parser')
+require File.join(File.dirname(__FILE__), 'service_proxy', 'version')
---
+++ lib/service_proxy/version.rb
@@ -0,0 +1,3 @@
+module ServiceProxy
+  VERSION = '1.0.0'
+end
